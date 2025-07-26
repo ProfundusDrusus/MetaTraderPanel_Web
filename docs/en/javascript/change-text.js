a8517cd3c5b057a6c84ba73e8f8860b62529df7c
Profundus Drusus <to_get_her@web.de>
2025-07-26 09:50:32 +0200

    Update change-text.js

--- docs/en/javascript/change-text.js
+++ docs/en/javascript/change-text.js
@@ -5,6 +5,7 @@ sub_texts.push("But we are - MetaTrader-Panel was designed with the one idea in 
 sub_texts.push("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore!");
 sub_texts.push("Has ne tritani atomorum conclusionemque, in dolorum volumus cotidieque eum. At vis choro legentur");
 
+var texts = new Array();
 texts.push("YOUR BROKER IS NOT YOUR FRIEND. HE IS YOUR COUNTERPART");
 texts.push("WHY YOUR BROKER KNOWS YOUR STOP LOSS");
 texts.push("A NEW STANDARD IN CFD RETAIL TRADING");
