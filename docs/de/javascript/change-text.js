497a93b929df969361ea34ae03b02dddf83faa69
Profundus Drusus <to_get_her@web.de>
2025-08-03 19:53:43 +0200

    No more flickering

--- docs/de/javascript/change-text.js
+++ docs/de/javascript/change-text.js
@@ -46,34 +46,34 @@ function changeTestimonials() {
 }
 
 var sub_texts = new Array();
-sub_texts.push("Schluss mit Stop-Loss Fischen. MetaTrader-Panel benutzt einen Stealth-Modus und dein Stop Loss ist für deinen Broker unsichtbar");
-sub_texts.push("Erfolgreich traden ist schwierig genug. Du solltest dich nicht mit banalen Dingen ablenken. Mit uns wird dein Trading erfolgreicher.");
-sub_texts.push("Das haben wir uns auch gefragt. Dann haben wir einfach einen Tick-Chart gebaut, aus dem man auch wirklich traden kann");
-sub_texts.push("Keine Sorge, du bist nicht allein. MetaTrader-Panel wurde mit der Idee entwickelt, den CFD-Handel für Endkunden fairer zu machen.");
-sub_texts.push("Erfahrung zählt. Unser Team vereint mehr als 120 Jahre professioneller Trading Erfahrung und wir wissen genau, was wir tun");
-sub_texts.push("Dein Broker ist auch dein Handelspartner. Verrätst du ihm deinen Target Point gibst du ihm einen entscheidenden Vorsprung");
-sub_texts.push("Profi-Trader mit jahrzehntelanger Handelserfahrung kombiniert mit jungen, erfolgshungrigen Talenten, das ist unser Geheimnis.");
-sub_texts.push("Natürlich nicht - wie denn auch? MetaTrader-Panel verfügt über eine eigene Datenbank und kann das. Kein Problem für uns");
+sub_texts[0]=("Schluss mit Stop-Loss Fischen. MetaTrader-Panel benutzt einen Stealth-Modus und dein Stop Loss ist für deinen Broker unsichtbar");
+sub_texts[1]=("Das haben wir uns auch gefragt. Dann haben wir einfach einen Tick-Chart gebaut, aus dem man auch wirklich traden kann");
+sub_texts[2]=("Keine Sorge, du bist nicht allein. MetaTrader-Panel wurde mit der Idee entwickelt, den CFD-Handel für Endkunden fairer zu machen.");
+sub_texts[3]=("Erfahrung zählt. Unser Team vereint mehr als 120 Jahre professioneller Trading Erfahrung und wir wissen genau, was wir tun");
+sub_texts[4]=("Dein Broker ist auch dein Handelspartner. Verrätst du ihm deinen Target Point gibst du ihm einen entscheidenden Vorsprung");
+sub_texts[5]=("Profi-Trader mit jahrzehntelanger Handelserfahrung kombiniert mit jungen, erfolgshungrigen Talenten, das ist unser Geheimnis.");
+sub_texts[6]=("Natürlich nicht - wie denn auch? MetaTrader-Panel verfügt über eine eigene Datenbank und kann das. Kein Problem für uns");
+sub_texts[7]=("Erfolgreich traden ist schwierig genug. Du solltest dich nicht mit banalen Dingen ablenken. Mit uns wird dein Trading erfolgreicher.");
 
 var texts = new Array();
-texts.push("WARUM VERRÄTST DU DEINEM BROKER DEIN STOP LOSS?");
-texts.push("DU TRADEST UND WIR KÜMMERN UNS UM DEN REST");
-texts.push("WARUM VERFÜGT MT5 ÜBER KEINEN GUTEN TICK-CHART?");
-texts.push("DEIN BROKER IST NICHT DEIN FREUND. ER IST DEIN HANDELS-PARTNER");
-texts.push("KONZENTRATION AUF DAS WESENTLICHE IST ENTSCHEIDEND");
-texts.push("WARUM KENNT DEIN HANDELS-PARTNER DEINEN TARGET POINT?");
-texts.push("INTELLIGENTER CODE, BLITZSCHNELL UND NEUESTE TECHNOLOGIE");
-texts.push("KANN MT5 FIBONACCI LEVELS OHNE DATEN ZEICHNEN?");
+texts[0]=("WARUM VERRÄTST DU DEINEM BROKER DEIN STOP LOSS?");
+texts[1]=("WARUM VERFÜGT MT5 ÜBER KEINEN GUTEN TICK-CHART?");
+texts[2]=("DEIN BROKER IST NICHT DEIN FREUND. ER IST DEIN HANDELS-PARTNER");
+texts[3]=("KONZENTRATION AUF DAS WESENTLICHE IST ENTSCHEIDEND");
+texts[4]=("WARUM KENNT DEIN HANDELS-PARTNER DEINEN TARGET POINT?");
+texts[5]=("INTELLIGENTER CODE, BLITZSCHNELL UND NEUESTE TECHNOLOGIE");
+texts[6]=("KANN MT5 FIBONACCI LEVELS OHNE DATEN ZEICHNEN?");
+texts[7]=("DU TRADEST UND WIR KÜMMERN UNS UM DEN REST");
 
 var images = [];
 images[0] = "assets/Panel_07x600.png";
-images[1] = "assets/Panel_04x600.png";
-images[2] = "assets/TickPanel_01.png";
-images[3] = "assets/Panel_07x600.png";
-images[4] = "assets/TickPanel_01.png";
-images[5] = "assets/Panel_07x600.png";
-images[6] = "assets/Panel_04x600.png";
-images[7] = "assets/Panel_08x600.png";
+images[1] = "assets/TickPanel_01.png";
+images[2] = "assets/Panel_07x600.png";
+images[3] = "assets/TickPanel_01.png";
+images[4] = "assets/Panel_07x600.png";
+images[5] = "assets/Panel_04x600.png";
+images[6] = "assets/Panel_08x600.png";
+images[7] = "assets/Panel_04x600.png";
 
 var links = [];
 links[0] = "features/position-props";
@@ -85,14 +85,22 @@ links[5] = "features/fibonacci";
 links[6] = "features/fibonacci";
 links[7] = "features/fibonacci";
 
-var point = 0;
-var point1 = 0;
+var point = -1;
+
 
 
 
 function changeHeaderText() {
 	
-	
+	console.log('Run ' + point);
+	if (point == -1) {
+		point++;
+      setTimeout(changeHeaderText, 12000);
+	  return;
+	} 
+	else {
+      setTimeout(changeHeaderText, 12000);
+	}
 
 	/* change picture*/
     $("#banner-content").fadeOut("slow", function () {
@@ -120,25 +128,16 @@ function changeHeaderText() {
 	 
 	/* change sub text*/   
 	$("#refresh-subtext").fadeOut("slow", function () {
-         $(this).html(sub_texts[point1]);
+         $(this).html(sub_texts[point]);
          $(this).fadeIn("slow");
       });
 	  
 
-	  
 	if (point < texts.length - 1) {
 	  point++;
 	} else {
 	  point = 0;
 	}
-	
-	if (point1 < sub_texts.length - 1) {
-      point1++;
-	} else {
-      point1 = 0;
-	}
-	
-	setTimeout(changeHeaderText, 12000)
 	
 }
 
